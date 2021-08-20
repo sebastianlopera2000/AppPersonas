cc1c401239bdf61889547dfe58e3c405279a5a34
Sebastian Lopera <lromansebastian@hotmail.com>
2021-08-20 00:53:46 -0500

    Initial commit

---
+++ AppPersonas/Controllers/HomeController.cs
@@ -0,0 +1,262 @@
+﻿using AppPersonas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppPersonas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            try
+            {
+                using (var db = new PersonasContext())
+                {
+                    var data = from ps in db.persona
+                               join p in db.pais on ps.paisNacim equals p.id
+                               select new PersonaBU()
+                               {
+                                   id = ps.id,
+                                   nombres = ps.nombres,
+                                   apellidos = ps.apellidos,
+                                   fechaNacim = ps.fechaNacim,
+                                   fechaFallec = ps.fechaFallec,
+                                   NombrePais = p.nombre
+                               };
+                    return View(data.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Agregar(persona p)
+        {
+            if (!ModelState.IsValid) return View();
+
+            try
+            {
+                using (var db = new PersonasContext())
+                {
+                    db.Actualizar_Persona(p.id, p.nombres, p.apellidos, p.fechaNacim, p.fechaFallec, p.paisNacim);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "El registro no ha sido ingresado.");
+                return View();
+            }
+        }
+
+        public ActionResult ListarPaises()
+        {
+            using (var db = new PersonasContext())
+            {
+                return PartialView(db.pais.ToList());
+            }
+        }
+
+        public ActionResult Editar(int id)
+        {
+            try
+            {
+                using (var db = new PersonasContext())
+                {
+                    persona p = db.persona.Find(id);
+                    return View(p);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error al mostrar los datos.");
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(persona p)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return View();
+
+                using (var db = new PersonasContext())
+                {
+                    db.Actualizar_Persona(p.id, p.nombres, p.apellidos, p.fechaNacim, p.fechaFallec, p.paisNacim);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "El registro no ha sido ingresado.");
+                return View();
+            }
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            using (var db = new PersonasContext())
+            {
+                persona p = db.persona.Find(id);
+                db.Eliminar_Persona(id);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public static string NombrePais(int id)
+        {
+            using (var db = new PersonasContext())
+            {
+                return db.pais.Find(id).nombre;
+            }
+        }
+
+        public ActionResult OrdenPais()
+        {
+            try
+            {
+                using (var db = new PersonasContext())
+                {
+                    var data = from ps in db.persona
+                               join p in db.pais on ps.paisNacim equals p.id
+                               orderby p.nombre, ps.fechaNacim
+                               select new PersonaBU()
+                               {
+                                   id = ps.id,
+                                   nombres = ps.nombres,
+                                   apellidos = ps.apellidos,
+                                   fechaNacim = ps.fechaNacim,
+                                   fechaFallec = ps.fechaFallec,
+                                   NombrePais = p.nombre
+                               };
+                    return View(data.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult OrdenEdad()
+        {
+            try
+            {
+                using (var db = new PersonasContext())
+                {
+                    var data = from ps in db.persona
+                               join p in db.pais on ps.paisNacim equals p.id
+                               orderby ps.fechaNacim
+                               select new PersonaBU()
+                               {
+                                   id = ps.id,
+                                   nombres = ps.nombres,
+                                   apellidos = ps.apellidos,
+                                   fechaNacim = ps.fechaNacim,
+                                   fechaFallec = ps.fechaFallec,
+                                   NombrePais = p.nombre
+                               };
+                    return View(data.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult Informe()
+        {
+            try
+            {
+                using (var db = new PersonasContext())
+                {
+                    var data = from t in (((from ps in db.persona
+                               group ps by new
+                               {
+                                   column1 = ps.fechaNacim.Year
+                               } into g
+                               select new
+                               {
+                                   agno = g.Key.column1,
+                                   nacidos = g.Count(p => p.fechaNacim != null),
+                                   muertos = 0
+                               }).Union(from ps in db.persona
+                                        group ps by new
+                                        {
+                                            column1 = ps.fechaFallec.Value.Year
+                                        } into g
+                                        select new
+                                        {
+                                            agno = g.Key.column1,
+                                            nacidos = 0,
+                                            muertos = g.Count(p => p.fechaFallec != null)
+                                        })))
+                                        group t by new
+                                        {
+                                            t.agno
+                                        } into g
+                               select new
+                               {
+                                   g.Key.agno,
+                                   nacidos = g.Sum(p => p.nacidos),
+                                   muertos = g.Sum(p => p.muertos)
+                               };
+                    return View(data.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public ActionResult OrdenFallecidos()
+        {
+            try
+            {
+                using (var db = new PersonasContext())
+                {
+                    var data = from ps in db.persona
+                               join p in db.pais on ps.paisNacim equals p.id
+                               where ps.fechaFallec != null
+                               orderby ps.fechaFallec
+                               select new PersonaBU()
+                               {
+                                   id = ps.id,
+                                   nombres = ps.nombres,
+                                   apellidos = ps.apellidos,
+                                   fechaNacim = ps.fechaNacim,
+                                   fechaFallec = ps.fechaFallec,
+                                   NombrePais = p.nombre
+                               };
+                    return View(data.ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ AppPersonas/Models/PersonaBU.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AppPersonas.Models
+{
+    public class PersonaBU
+    {
+        public int id { get; set; }
+        [Required]
+        [Display(Name = "Nombres")]
+        public string nombres { get; set; }
+        [Required]
+        [Display(Name = "Apellidos")]
+        public string apellidos { get; set; }
+        [Required]
+        [Display(Name = "Fecha de Nacimiento")]
+        public System.DateTime fechaNacim { get; set; }
+        [Display(Name = "Fecha de Fallecimiento")]
+        public Nullable<System.DateTime> fechaFallec { get; set; }
+        [Required]
+        [Display(Name = "País de Nacimiento")]
+        public int paisNacim { get; set; }
+
+        public string NombrePais { get; set; }
+
+        public string agno { get; set; }
+
+        public string nacimientos { get; set; }
+
+        public string muertes { get; set; }
+
+    }
+    [MetadataType(typeof(PersonaBU))]
+    public partial class Persona
+    {
+        public string NombrePais { get; set; }
+    }
+}
